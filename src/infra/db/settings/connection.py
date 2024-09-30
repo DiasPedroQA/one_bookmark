d278510b48cd3900d63aa3c8f87b483d2ac3f354
DiasPedroQA <diaspedro.dev@gmail.com>
2024-09-29 22:42:34 -0300

    001
    Signed-off-by: DiasPedroQA <diaspedro.dev@gmail.com>

--- src/infra/db/settings/connection.py
+++ src/infra/db/settings/connection.py
@@ -1,4 +1,25 @@
+from sqlalchemy import create_engine, text
+
 
 class DBConnectionHandler:
     def __init__(self) -> None:
-        pass
+        self.__connection_string = "{}://{}:{}@{}:{}/{}".format(
+            "mysql+pymysql",
+            "root",
+            "#R1040grau$",
+            "localhost",
+            "3306",
+            "file_manager",
+        )
+        self.__engine = self.__create_database_engine()
+
+    def __create_database_engine(self) -> None:
+        return create_engine(self.__connection_string)
+
+    def get_engine(self) -> None:
+        return self.__engine
+
+    def execute_query(self, query: str):
+        with self.__engine.connect() as connection:
+            result = connection.execute(text(query))
+            return result.fetchall()
